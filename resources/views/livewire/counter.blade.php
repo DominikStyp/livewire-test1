0144a95dcc644d3ff75b8185d85c4889d4af9b63
Dominik Stypuła <dominik.stypula@polcode.net>
2022-12-17 23:05:07 +0100

    Livewire initial

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+
+<div style="text-align: center" id="counterComponent">
+    <button wire:click="increment">+</button>
+    <button wire:click="decrement">-</button>
+    <h1>{{ $count }}</h1>
+</div>
